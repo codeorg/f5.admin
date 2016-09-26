e3c87165f0730c51e458f0246ac15e3cf271b6de
codeorg <codeasy@163.com>
2016-09-26 13:53:45 +0800

    v1.0.0

---
+++ app.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Codeorg.com on 2016/9/26.
+ */
+"use strict";
+let env = process.env.NODE_ENV || 'production';
+let root=(env=="production")?"dist":"src";
+let port=5000;
+let Koa = require('koa');
+var send = require('koa-send');
+const app = new Koa();
+
+app.use(async (ctx, next)=>{
+    if(/([\.]+)/gi.test(ctx.path)){
+        await send(ctx, "/"+root+ctx.path);
+    }else{
+        await send(ctx, "/"+root+"/index.html");
+    }
+});
+
+app.listen(port);
+console.log("服务启动端口："+port)
+
+
+
+
