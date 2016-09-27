a7346a353e56417b281abe0cd40efc94c739f075
codeorg <codeasy@163.com>
2016-09-27 10:56:39 +0800

    v1.0.0

--- src/js/router.js
+++ src/js/router.js
@@ -59,6 +59,9 @@ define([
             }
         }, config);
     }
+
+
+
     function ModuleInjector(app,$stateProvider, $controllerProvider, $compileProvider, $filterProvider, $provide,$animateProvider) {
 
         //-------动态注入　开始-----------------------------------------------------------------
@@ -89,6 +92,23 @@ define([
 
             return function (modules) {
                 var mods=[];
+                if (!Array.prototype.indexOf) {
+                    Array.prototype.indexOf = function (elt /*, from*/) {
+                        var len = this.length >>> 0;
+                        var from = Number(arguments[1]) || 0;
+                        from = (from < 0)
+                            ? Math.ceil(from)
+                            : Math.floor(from);
+                        if (from < 0)
+                            from += len;
+                        for (; from < len; from++) {
+                            if (from in this &&
+                                this[from] === elt)
+                                return from;
+                        }
+                        return -1;
+                    };
+                }
                 function loadModules(modulesToLoad) {
                     angular.forEach(modulesToLoad, function (module) {
                         if (mods.indexOf(module) == -1) {
